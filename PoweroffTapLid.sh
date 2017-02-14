0a5cbf891e2f15c0d8d078dc3d0fd18e38b033f4
vdt-mik <vdt-mik@alterpost.org>
2017-02-14 21:43:51 +0200

    Fix PoweroffTapLid script

--- PoweroffTapLid.sh
+++ PoweroffTapLid.sh
@@ -1,7 +1,7 @@
-!#/bin/bash
+#!/bin/bash
 #Бекапим файл конфига
-cp /etc/systemd/logind.conf /etc/systemd/logind.conf.bak
+sudo cp /etc/systemd/logind.conf /etc/systemd/logind.conf.bak
 #Включаем выключение при закрытии крышки
-sed -e 's/#HandleLidSwitch=suspend/HandleLidSwitch=poweroff/g' /etc/systemd/logind.conf
+sudo sed -i 's/#HandleLidSwitch=suspend/HandleLidSwitch=poweroff/' /etc/systemd/logind.conf
 #Перезапускаем службу
-systemctl restart systemd-logind.service
+sudo systemctl restart systemd-logind.service
